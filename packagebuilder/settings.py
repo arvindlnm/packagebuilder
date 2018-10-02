64e9ff3e2c41a4701b86ff0373530c368121d405
Arvind Thakur <athakur@tyro.com>
2018-10-02 13:18:53 +1000

    personal chnages

--- packagebuilder/settings.py
+++ packagebuilder/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 import django_heroku
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
