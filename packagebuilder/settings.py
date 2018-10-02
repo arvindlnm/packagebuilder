248aace72cfc7eedde0b0e0dd7c88dba9861ff95
Arvind Thakur <athakur@tyro.com>
2018-10-02 12:28:50 +1000

    personal chnages

--- packagebuilder/settings.py
+++ packagebuilder/settings.py
@@ -96,12 +96,12 @@ TEMPLATE_DIRS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 # STATIC_ROOT = 'staticfiles'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(__file__), 'static'),
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
